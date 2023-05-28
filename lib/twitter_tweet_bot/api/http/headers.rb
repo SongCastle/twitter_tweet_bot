41610ddc7af58f31c37a2ac514f481945a7df09b
uk <47803499+SongCastle@users.noreply.github.com>
2023-05-28 16:48:51 +0900

    Change `HTTP::Headers#basic_authorization_header` I / F
    ```
    # Before
    basic_authorization_header('user:pass')
    
    # After
    basic_authorization_header('user', 'pass')
    ```

--- lib/twitter_tweet_bot/api/http/headers.rb
+++ lib/twitter_tweet_bot/api/http/headers.rb
@@ -1,3 +1,4 @@
+require 'base64'
 require 'uri'
 require 'net/http'
 
@@ -8,12 +9,23 @@ module TwitterTweetBot
         BASIC_AUTHORIZATION = 'Basic %<credentials>s'.freeze
         BEARER_AUTHORIZATION = 'Bearer %<credentials>s'.freeze
 
-        def basic_authorization_header(credentials)
-          { authorization: format(BASIC_AUTHORIZATION, credentials: credentials) }
+        def basic_authorization_header(user, password)
+          {
+            authorization: format(
+              BASIC_AUTHORIZATION,
+              credentials: authorization_credentials(user, password)
+            )
+          }
         end
 
         def bearer_authorization_header(credentials)
           { authorization: format(BEARER_AUTHORIZATION, credentials: credentials) }
+        end
+
+        private
+
+        def authorization_credentials(user, password)
+          Base64.strict_encode64("#{user}:#{password}")
         end
       end
     end
