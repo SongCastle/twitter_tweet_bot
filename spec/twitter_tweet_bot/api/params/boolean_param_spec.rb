5069a8f4b6e792b7807903450c6b9b0a22fce0b6
uk <47803499+SongCastle@users.noreply.github.com>
2023-07-09 04:53:09 +0900

    Add `API::Params::BooleanParam`

---
+++ spec/twitter_tweet_bot/api/params/boolean_param_spec.rb
@@ -0,0 +1,31 @@
+require 'twitter_tweet_bot/api/params/boolean_param'
+
+RSpec.describe TwitterTweetBot::API::Params::BooleanParam do
+  describe '::build' do
+    subject { described_class.build('foo', value) }
+
+    context 'when a value is true' do
+      let(:value) { true }
+
+      it 'returns params for an API request' do
+        is_expected.to eq({ 'foo' => value })
+      end
+    end
+
+    context 'when a value is false' do
+      let(:value) { false }
+
+      it 'returns params for an API request' do
+        is_expected.to eq({ 'foo' => value })
+      end
+    end
+
+    context 'when a value is NOT TrueClass/FalseClass' do
+      let(:value) { 'true' }
+
+      it 'returns params for an API request' do
+        is_expected.to eq({})
+      end
+    end
+  end
+end
